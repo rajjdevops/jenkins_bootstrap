88b33824074dfc3a7a4a6caadb51ed58ed1e0bbf
rajjdevops <65726128+rajjdevops@users.noreply.github.com>
2021-11-05 22:49:28 +1100

    Update s3bucket.groovy

--- jobs/bucket/s3bucket.groovy
+++ jobs/bucket/s3bucket.groovy
@@ -1,32 +1,15 @@
-pipelineJob("folder name/job name") {
-        description("<h3> this job will create</h3> ")
-      
-        logRotator {
-            numTokeep(10)
+pipelineJob('shell') {
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url('https://github.com/rajjdevops/jenkins_bootstrap.git')
+          }
+          branch('*/master')
         }
-
-        properties {
-                      disableConcurrentBuilds()
-        }
-
-        parameters { 
-            choiceParam('DEPLOYMENT_BRANCH', ['new','master'], 'select branch to deploy')  
-        }
-  
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        remote {
-                            url ("https://github.com/rajjdevops/jenkins_bootstrap.git")
-                            
-                      
-                        }
-                        branch('\$DEPLOYMENT_BRANCH')    
-                    }
-                }
-                scriptPath('shell/Jenkinsfile')
-            }
-        }
-
+      }
+      lightweight()
     }
+  }
+}
